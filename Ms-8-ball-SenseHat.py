5fe4d7665d098a6d6cd77138c1bfac37d9f19792
mschreckenghaust792 <mschreckenghaust792@lsr7.net>
2018-12-06 08:58:08 -0600

    added a file

---
+++ Ms-8-ball-SenseHat.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+from random import choice
+from time import sleep
+print("Ask a question")
+sleep(3)
+replies = ['Signs point to yes',
+           'Without a doubt',
+           'Do not count on it',
+           'looking good',
+           'cannot predict now',
+           'It is decidendly so',
+           'Outlook not so good'
+           ]
+print (choice(replies))
+
+
+
